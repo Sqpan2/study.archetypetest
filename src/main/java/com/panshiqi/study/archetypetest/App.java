f98682ac613200ec0b77a44d05b4e55a99df295c
Sqpan2 <951514058@qq.com>
2018-04-02 16:26:45 +0800

    自定义maven骨架demo--单个项目
    自定义maven骨架demo，用于快速创建一个项目

---
+++ src/main/java/com/panshiqi/study/archetypetest/App.java
@@ -0,0 +1,13 @@
+package com.panshiqi.study.archetypetest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello archrtype... " );
+    }
+}
